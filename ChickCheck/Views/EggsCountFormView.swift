c045bd44b5705cc946ea2c795ab7f72a2a77dbc4
jirijanecek <jiri.janecek@yahoo.com>
2024-05-15 22:31:43 +0200

    Moved views to its own files

---
+++ ChickCheck/Views/EggsCountFormView.swift
@@ -0,0 +1,41 @@
+//
+//  EggsCountFormView.swift
+//  ChickCheck
+//
+//  Created by Jiri Janecek on 15.05.2024.
+//
+
+import SwiftUI
+
+struct EggsCountFormView: View {
+    @State private var eggsCount: String = ""
+    
+    var body: some View {
+        HStack {
+            Text("Počet vajíček")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(.bold)
+            
+            Spacer()
+        }
+        
+        Spacer()
+            .frame(height: 10)
+        
+        TextField("Zadejte počet vajíček", text: $eggsCount)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .padding(.vertical, 12)
+            .padding(.horizontal, 10)
+            .background(Color.pinkRose)
+            .cornerRadius(5)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+            )
+    }
+}
+
+#Preview {
+    EggsCountFormView()
+}
