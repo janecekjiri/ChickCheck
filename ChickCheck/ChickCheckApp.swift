759f69c9e053eb915ec9793751e8a51ecb2a8da8
jirijanecek <jiri.janecek@yahoo.com>
2024-05-15 09:25:19 +0200

    Initial Commit

---
+++ ChickCheck/ChickCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickCheckApp.swift
+//  ChickCheck
+//
+//  Created by Jiri Janecek on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChickCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
