db41b8b7d3b1519b689eab38f1d082287da81fdf
jirijanecek <jiri.janecek@yahoo.com>
2024-05-22 08:58:15 +0200

    Added navigation and alert to detail screen

--- ChickCheck/Screens/Detail/DetailView.swift
+++ ChickCheck/Screens/Detail/DetailView.swift
@@ -8,35 +8,85 @@
 import SwiftUI
 
 struct DetailView: View {
+    @State private var showAlert = false
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        VStack(spacing: 0) {
-            DateFormView()
-            
-            Spacer()
-                .frame(height: 20)
-            
-            EggsCountFormView()
-            
-            Spacer()
-            
-            Button(action: {
+        NavigationStack {
+            VStack(spacing: 0) {
+                DateFormView()
                 
-            }, label: {
-                Text("save")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.white)
-                    .padding(.vertical, 12)
-            })
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .frame(height: 50)
-            .background(Color.green)
-            .cornerRadius(25)
-            
-            Spacer()
-                .frame(height: 20)
+                Spacer()
+                    .frame(height: 20)
+                
+                EggsCountFormView()
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("save")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 12)
+                })
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .frame(height: 50)
+                .background(Color.green)
+                .cornerRadius(25)
+                
+                Spacer()
+                    .frame(height: 20)
+            }
+            .padding(.horizontal, 20)
+            // TODO: Upravit titulek na "Vytvořit záznam" a "Upravit záznam"
+            .navigationTitle("detail_screen_title")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                // TODO: Zobraz detail pouze, pokud jde o úpravu, nikoliv vytvoření záznamu
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(
+                        action: {
+                            showAlert = true
+                        },
+                        label: {
+                            Image(systemName: "trash")
+                                .renderingMode(.template)
+                                .foregroundStyle(Color(.label))
+                    })
+                }
+            }
         }
-        .padding(.horizontal, 20)
+        .alert(
+            "delete_detail_alert_title",
+            isPresented: $showAlert,
+            actions: {
+                Button(
+                    role: .destructive,
+                    action: {
+                        dismiss()
+                        // TODO: Smaž detail
+                    },
+                    label: {
+                        Text("yes")
+                })
+                
+                Button(
+                    role: .cancel,
+                    action: {
+                        dismiss()
+                    },
+                    label: {
+                        Text("no")
+                    }
+                )
+            },
+            message: {
+                Text("delete_detail_alert_message")
+            }
+        )
     }
 }
 
