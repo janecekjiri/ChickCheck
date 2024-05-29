c1426599e2e2db60963daf0adabbb965821efe1d
jirijanecek <jiri.janecek@yahoo.com>
2024-05-29 08:55:33 +0200

    Added code for selecting date

--- ChickCheck/Views/DateFormView.swift
+++ ChickCheck/Views/DateFormView.swift
@@ -8,60 +8,75 @@
 import SwiftUI
 
 struct DateFormView: View {
+    @State private var showDatePicker = false
+    @State private var date = Date.now
+    
     var body: some View {
-        // Datum label
-        HStack {
-            Text("date")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .fontWeight(.bold)
-            
-            Spacer()
-        }
-        
-        Spacer()
-            .frame(height: 10)
-        
-        // Pole pro datum, tlačítko
-        HStack(spacing: 10) {
-            // Pole pro datum
-            ZStack {
-                Color.pinkRose
-                    .frame(height: 50)
-                    .cornerRadius(5)
+        VStack(spacing: 0) {
+            // Datum label
+            HStack {
+                Text("date")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.bold)
                 
-                HStack {
-                    Text(verbatim: "9.5.2024")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .padding(.vertical, 12)
-                        .padding(.horizontal, 10)
-                    
-                    Spacer()
-                }
+                Spacer()
             }
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-            )
-            
-            // Tlačítko
-            Image("calendar")
-                .resizable()
-                .frame(width: 30, height: 30)
-                .padding(10)
-                .background(Color.pinkRose)
-                .cornerRadius(5)
-        }
-        
-        Spacer()
-            .frame(height: 5)
-        
-        // Text pod polem
-        HStack {
-            Text("date_picker_helper_message")
-                .font(.footnote)
             
             Spacer()
+                .frame(height: 10)
+            
+            // Pole pro datum, tlačítko
+            HStack(spacing: 10) {
+                // Pole pro datum
+                ZStack {
+                    Color.pinkRose
+                        .frame(height: 50)
+                        .cornerRadius(5)
+                    
+                    HStack {
+                        Text(verbatim: self.date.formatted(.dateTime.day(.defaultDigits).month().year()))
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .padding(.vertical, 12)
+                            .padding(.horizontal, 10)
+                        
+                        Spacer()
+                    }
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                )
+                
+                // Tlačítko
+                Image("calendar")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .padding(10)
+                    .background(Color.pinkRose)
+                    .cornerRadius(5)
+                    .onTapGesture {
+                        withAnimation {
+                            self.showDatePicker.toggle()
+                        }
+                    }
+            }
+            
+            Spacer()
+                .frame(height: 5)
+            
+            // Text pod polem
+            HStack {
+                Text("date_picker_helper_message")
+                    .font(.footnote)
+                
+                Spacer()
+            }
+            
+            if self.showDatePicker {
+                DatePicker("", selection: $date, in: ...Date.now, displayedComponents: .date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+            }
         }
     }
 }
