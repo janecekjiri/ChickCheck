7e18e95d5d6d9a1c29fbc3a3eba5798642675807
jirijanecek <jiri.janecek@yahoo.com>
2024-05-29 09:23:24 +0200

    Added ability to insert amount of laid eggs

--- ChickCheck/Views/EggsCountFormView.swift
+++ ChickCheck/Views/EggsCountFormView.swift
@@ -8,31 +8,48 @@
 import SwiftUI
 
 struct EggsCountFormView: View {
-    @State private var eggsCount: String = ""
+    @State private var eggsCount: Int?
+    @FocusState private var isEggsCountFieldActive: Bool
     
     var body: some View {
-        HStack {
-            Text("eggs_count")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .fontWeight(.bold)
+        VStack {
+            HStack {
+                Text("eggs_count")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.bold)
+                
+                Spacer()
+            }
             
             Spacer()
+                .frame(height: 10)
+            
+            TextField("insert_eggs_count", value: $eggsCount, format: .number)
+                .keyboardType(.numberPad)
+                .focused($isEggsCountFieldActive)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .padding(.vertical, 12)
+                .padding(.horizontal, 10)
+                .background(Color.pinkRose)
+                .cornerRadius(5)
+                .toolbar {
+                    // Using empty Toolbar Item here to push Cancel button to the right
+                    ToolbarItem(id: "empty_item", placement: .keyboard) {
+                        Text(verbatim: "")
+                    }
+                    
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button("hide_keyboard") {
+                            isEggsCountFieldActive = false
+                        }
+                    }
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+                )
         }
-        
-        Spacer()
-            .frame(height: 10)
-        
-        TextField("insert_eggs_count", text: $eggsCount)
-            .font(.title2)
-            .fontWeight(.semibold)
-            .padding(.vertical, 12)
-            .padding(.horizontal, 10)
-            .background(Color.pinkRose)
-            .cornerRadius(5)
-            .overlay(
-                RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color.black, lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-            )
     }
 }
 
