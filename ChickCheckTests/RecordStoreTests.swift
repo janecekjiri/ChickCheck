f13b7fa4700839049d9407b701161eeb0739156d
jirijanecek <jiri.janecek@yahoo.com>
2024-06-07 16:14:01 +0200

    Added testing for record store

--- ChickCheckTests/RecordStoreTests.swift
+++ ChickCheckTests/RecordStoreTests.swift
@@ -19,7 +19,65 @@ final class RecordStoreTests: XCTestCase {
     
     override func tearDown() {
         self.sut = nil
+        UserDefaults.standard.setValue(nil, forKey: "recordsKey")
         super.tearDown()
+    }
+    
+    func testWhenInitialized_areRecordsEmpty() {
+        XCTAssertEqual(self.sut.recordsExternal.count, 0)
+    }
+    
+    func testWhenSavedRecord_isThereOneRecord() {
+        let detail = DetailModel()
+        detail.count = 15
+        sut.saveRecord(detail)
+        XCTAssertEqual(sut.recordsExternal.count, 1)
+    }
+    
+    func testWhenRemovedRecods_areRecordsEmpty() {
+        let detail = DetailModel()
+        detail.count = 15
+        sut.saveRecord(detail)
+        sut.removeRecord(detail)
+        XCTAssertEqual(sut.recordsExternal.count, 0)
+    }
+    
+    func testWhenSavingRecordWithAlreadySavedDate_isNumberOfRecordsSame() {
+        let detail1 = self.makeDetail(count: 15, daysAgo: 0)
+        let detail2 = self.makeDetail(count: 10, daysAgo: 1)
+        let detail3 = self.makeDetail(count: 9, daysAgo: 0)
+        sut.saveRecord(detail1)
+        sut.saveRecord(detail2)
+        sut.saveRecord(detail3)
+        XCTAssertEqual(sut.recordsExternal.count, 2)
+    }
+    
+    func testWhenAllRecordsAreRemoved_areRecordsEmpty() {
+        let detail1 = self.makeDetail(count: 15, daysAgo: 0)
+        let detail2 = self.makeDetail(count: 10, daysAgo: 1)
+        let detail3 = self.makeDetail(count: 9, daysAgo: 2)
+        sut.saveRecord(detail1)
+        sut.saveRecord(detail2)
+        sut.saveRecord(detail3)
+        sut.removeAll()
+        XCTAssertEqual(sut.recordsExternal.count, 0)
+    }
+    
+    func testWhenSavingRecordWithAlreadySavedDate_isOldRecordUpdated() {
+        let detail1 = self.makeDetail(count: 15, daysAgo: 0)
+        let detail2 = self.makeDetail(count: 10, daysAgo: 1)
+        let detail3 = self.makeDetail(count: 9, daysAgo: 0)
+        sut.saveRecord(detail1)
+        sut.saveRecord(detail2)
+        sut.saveRecord(detail3)
+        XCTAssertEqual(sut.recordsExternal[0].count, 9)
+    }
+    
+    private func makeDetail(count: Int, daysAgo: Int) -> DetailModel {
+        let detail = DetailModel()
+        detail.count = count
+        detail.date = Calendar.current.date(byAdding: .day, value: -daysAgo, to: Date.now) ?? Date.now
+        return detail
     }
 
 }
